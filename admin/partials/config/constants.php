2e71c5ce94ae2504a6b8debad684d83353950140
Ayishabanu <71723925+Ayishabanu@users.noreply.github.com>
2021-05-04 20:22:47 -1000

    Add files via upload

---
+++ admin/partials/config/constants.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+define("SITEURL","http://ayisha-product.infinityfreeapp.com/");
+$db=mysqli_connect('sql206.epizy.com','epiz_28331123','gMmxjAT1Dv1','epiz_28331123_productDB');
+?>
