f12809583cbe4bde7d5545271d7210d205212c4a
Ayishabanu <71723925+Ayishabanu@users.noreply.github.com>
2021-05-04 20:25:34 -1000

    Add files via upload

---
+++ partial-front/footer.php
@@ -0,0 +1,28 @@
+ <!-- social Section Starts Here -->
+    <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/ayishabanu-syedibrahim-72145218b/"><img src="https://img.icons8.com/fluent/50/000000/linkedin.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/invites/contact/?i=1wkfmuvnib062&utm_content=kj5rf96"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://github.com/Ayishabanu"><img src="https://img.icons8.com/fluent/48/000000/github.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="https://ayishabanu.github.io/portfolio/">Ayishabanu</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
